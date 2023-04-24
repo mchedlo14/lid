12d5a5a2089d98b602bbd8c131260aaa3516b36e
mchedlo14 <golocogo4@gmail.com>
2023-04-24 09:35:05 +0400

    lid txt

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+
+  return (
+    <div>
+          <a href="leadership.docx.txt" download={'lid'}>
+            <button className="cv-btn">Dow Lid</button>
+          </a>
+    </div>
+  );
+}
+
+export default App;
