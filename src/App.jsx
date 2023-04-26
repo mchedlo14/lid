6eec943e28dda32e59a39594939121a038bbcf0a
mchedlo14 <golocogo4@gmail.com>
2023-04-26 11:58:23 +0400

    added xls file

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
           <a href="leadership.docx.txt" download={'lid'}>
             <button className="cv-btn">Dow Lid</button>
           </a>
+          <a href="exx.xlsx" download={'excel'}>
+            <button className="cv-btn">Dow Excel</button>
+          </a>
     </div>
   );
 }
